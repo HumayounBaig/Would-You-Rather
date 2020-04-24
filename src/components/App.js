9a29b72ce901326df6d072565db615d2eaf289f2
HumayounBaig <humayoun.mani@gmail.com>
2020-04-25 00:28:52 +0500

    type: feat
    body: added invalid path route

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import CreatePoll from './CreatePoll';
 import Nav from './Nav';
 import LeaderBoard from './LeaderBoard';
 import InvalidPath from './InvalidPath';
+import QuestionCard from './QuestionCard'
 class App extends Component {
 
   componentDidMount() {
@@ -21,30 +22,37 @@ class App extends Component {
     return (
       <div className="App">
 
-          {
-            authUser === null ? (
-              <Route render ={()=> (<Login />)} />
-            )
-            :
-            (
-              <>
-                <Nav />
+        {
+          authUser === null ? (
+            <Route render={() => (<Login />)} />
+          )
+          :
+          (
+            <>
+              <Nav />
+              <Home>
                 <Switch>
                   <Route exact path="/" component={Dashboard} />
                   <Route path="/new-poll" component={CreatePoll} />
                   <Route path="/leaderboard" component={LeaderBoard} />
-                  <Route component={InvalidPath}  />
+                  <Route path="/questions/:questionId" component={QuestionCard} />
+                  <Route path="/questions/invalidId" component={InvalidPath} />
+                  <Route component={InvalidPath} />
                 </Switch>
-                  
-              </>
-            )
-
-          }
-
+              </Home>
+            </>
+          )
+        }
       </div>
     );
   }
 }
+
+const Home = ({ children }) => (
+  <div>
+    {children}
+  </div>
+)
 
 function mapStateToProps({ authUser }) {
   return (
