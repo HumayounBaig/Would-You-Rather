d8dc94bbb2c89244f807d8076c70ba3e2750b26c
HumayounBaig <humayoun.mani@gmail.com>
2020-04-25 04:38:53 +0500

    type: removed dev tools extension

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import rootReducer from './redux/reducers/index';
 import middleware from './middleware'
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore } from 'redux';
-const store = createStore(rootReducer, composeWithDevTools(middleware))
+const store = createStore(rootReducer, middleware)
 
 export default store;
