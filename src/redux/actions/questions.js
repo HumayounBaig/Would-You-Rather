155c49a26172e21870d7cf191f5a2599e50b1bbe
Humayoun Baig <humayoun@Humayouns-MacBook-Air.local>
2020-04-24 06:49:36 +0500

    added new question actions

--- src/redux/actions/questions.js
+++ src/redux/actions/questions.js
@@ -1,8 +1,30 @@
 export const GET_QUESTIONS = 'GET_QUESTIONS';
-
+export const ADD_QUESTION = 'ADD_QUESTION'
+import { saveQuestion } from '../../utils/apiHandler'
+import { addQuestion } from '../actions/users'
 export function getQuestions(questions) {
 	return{
         type: GET_QUESTIONS,
         questions
     }
 }
+
+function appendNewQuestion(question) {
+    return {
+      type: ADD_QUESTION,
+      question
+    };
+  }
+
+export function addQuestion(question){
+    return dispatch => {
+        return saveQuestion(question).then(
+            response => {
+                dispatch(appendNewQuestion(question));
+                dispatch(addQuestion(question));
+            }
+        )
+    }
+     
+}
+
--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,8 +1,20 @@
 export const GET_USERS = 'GET_USERS';
+export const ADD_QUESTION = 'ADD_QUESTION';
+export const ADD_ANSWER = 'ADD_ANSWER';
+export const ADD_ANSWER_TO_QUESTION = 'ADD_ANSWER_TO_QUESTION';
 
 export function getUsers(users) {
 	return{
         type: GET_USERS,
         users
+    }
+}
+
+//Appends Question to user who created it
+export function addQuestion({ id, author}) {
+	return{
+        type: ADD_QUESTION,
+        id,
+        author
     }
 }
