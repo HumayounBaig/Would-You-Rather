2ea8289e5cdfe578c2831b88eb19c159a26b81b3
Humayoun Baig <humayoun@Humayouns-MacBook-Air.local>
2020-04-24 05:52:25 +0500

    type: feat
    body: added dashboard component which displays polls
    and top bar

---
+++ src/components/Dashboard.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { handleInitialData } from '../redux/actions/shared';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import NavBar from './Nav'
+import classnames from 'classnames';
+import QuestionCard from './QuestionCard'
+import "../styles/App.css"
+
+function Dashboard(props) {
+
+  const { questionsData } = props
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if (activeTab !== tab) setActiveTab(tab);
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <Row className="Dashboard">
+        <Col className="col-centered" md={{ size: 6, offset: 3 }}>
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggle('1'); }}
+              >
+                Unanswered Questions
+            </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggle('2'); }}
+              >
+                Answered Questions
+            </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              {
+                questionsData.unanswered.map((question) => (
+                  <QuestionCard 
+                    key={question.id}
+                    userId={question.author}
+                    question={question}
+                    isAnswered={false}
+                  />
+                ))
+              }
+            </TabPane>
+            <TabPane tabId="2">
+              {
+                questionsData.answered.map((question) => (
+                  <QuestionCard 
+                    key={question.id}
+                    userId={question.author}
+                    question={question}
+                    isAnswered={true}
+                  />
+                ))
+              }
+            </TabPane>
+          </TabContent>
+        </Col>
+      </Row>
+    </div>
+
+  );
+}
+
+function mapStateToProps({ authUser, users, questions }) {
+  const answeredIds = Object.keys(users[authUser.value].answers);
+  const answered = Object.values(questions)
+    .filter(question => answeredIds.includes(question.id))
+    .sort((a, b) => b.timestamp - a.timestamp);
+  const unanswered = Object.values(questions)
+    .filter(question => !answeredIds.includes(question.id))
+    .sort((a, b) => b.timestamp - a.timestamp);
+
+  return {
+    questionsData: {
+      answered,
+      unanswered
+    }
+  }
+}
+
+Dashboard.propTypes = {
+  questionsData: PropTypes.object.isRequired
+}
+
+export default connect(
+  mapStateToProps,
+)(Dashboard);
